05499958cfa7c4af7b1f661934669a69d7431293
Thessalene <thessalene@users.noreply.github.com>
2020-10-14 13:27:22 -0400

    Prestations card animations mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'JDBTPWeb';
-  
+
+  /* Prestation component data */
+  image1 = "../../assets/icons/prestations/construction.png";
+  title1 = "Constructions";
+  description1="Nous réalisons vos rêves les plus fou de A à Z.";
+
+  image2 = "../../assets/icons/prestations/maintenance.png";
+  title2 = "Rénovations";
+  description2="Pourquoi construire quand on peut rénover, améliorer, agrandir ?";
+
+  image3 = "../../assets/icons/prestations/worker.png";
+  title3 = "Expertise";
+  description3="Une équipe pluridisciplinaire à votre écoute.";
+
+  image4 = "../../assets/icons/prestations/quality.png";
+  title4 = "Qualité recherchée";
+  description4 ="La qualité ? On vous l'offre au travers de nos réalisations. L'expérience ? Notre point fort";
 
   
 }
--- src/app/prestation/prestation.component.ts
+++ src/app/prestation/prestation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prestation',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prestation.component.css']
 })
 export class PrestationComponent implements OnInit {
+  @Input() image : string;
+  @Input() title : string;
+  @Input() description : string;
 
   constructor() { }
 
